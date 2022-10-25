9beee691e2f63bd0721be8de2573f4b4c6f6429a
naufalmng <naufalmng@gmail.com>
2022-10-25 13:30:35 +0700

    mvvm template

--- app/src/main/java/org/d3ifcool/catok/ui/MainActivity.kt
+++ app/src/main/java/org/d3ifcool/catok/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package org.d3ifcool.catok
+package org.d3ifcool.catok.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3ifcool.catok.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
